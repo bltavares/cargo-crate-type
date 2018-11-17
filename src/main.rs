fbae648157c5b8b806d1c13ceb1f9b6b519d0da6
Bruno Tavares <connect+github@bltavares.com>
2018-11-17 19:21:22 -0200

    Fix subcomand matches as cargo extension

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::fs::read_to_string;
 use std::fs::write;
 
@@ -9,20 +9,27 @@ use lib::set_crate_type;
 use lib::CrateType;
 
 fn main() {
-    let matches = App::new("cargo crate-type")
+    let app = App::new("cargo crate-type")
+        .bin_name("cargo")
         .version("1.0")
-        .about("Edits the lib crate type to help building cross-platform libs")
-        .arg(
-            Arg::with_name("target")
-                .required(true)
-                .possible_values(&["dynamic", "static"]),
-        ).arg(
-            Arg::with_name("file")
-                .short("f")
-                .long("file")
-                .required(false)
-                .default_value("Cargo.toml"),
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("crate-type")
+                .about("Edits the lib crate type to help building cross-platform libs")
+                .arg(
+                    Arg::with_name("target")
+                        .required(true)
+                        .possible_values(&["dynamic", "static"]),
+                ).arg(
+                    Arg::with_name("file")
+                        .short("f")
+                        .long("file")
+                        .required(false)
+                        .default_value("Cargo.toml"),
+                ),
         ).get_matches();
+
+    let matches = app.subcommand_matches("crate-type").unwrap();
 
     let filename = matches.value_of("file").unwrap();
     let manifest = read_to_string(filename).expect("Not possible to read the given Cargo.toml");
