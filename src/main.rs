c197f9746e67413ded38a2812dece73c850da7f7
Bruno Tavares <connect+github@bltavares.com>
2018-11-17 19:22:27 -0200

    Adjust version

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use lib::CrateType;
 fn main() {
     let app = App::new("cargo crate-type")
         .bin_name("cargo")
-        .version("1.0")
+        .version("0.1.0")
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(
             SubCommand::with_name("crate-type")
